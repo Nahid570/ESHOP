239289a85ba7be9ca053f2c6e6bdc594339f34ee
Nahid570 <farajinahid2@gmail.com>
2023-06-13 13:43:25 +0600

    reset pass path updated

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -211,7 +211,7 @@ exports.forgotPasswordToken = asyncHandler(async (req, res) => {
   try {
     const token = await user.createPasswordResetToken();
     await user.save();
-    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='https://eshop-56kb11o21-nahid570.vercel.app/api/user/reset-password/${token}'>Click Here</a>`;
     const data = {
       to: email,
       text: "Hey User",
